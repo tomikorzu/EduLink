3da153235896358920132aff9eb17d41ebfefda9
tomikorzu <167248198+tomikorzu@users.noreply.github.com>
2024-04-30 06:05:51 -0300

    actualizo

--- configs/js/main.js
+++ configs/js/main.js
@@ -1,68 +1,64 @@
-
-
 (function() {
-    "use strict";
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
   
-    /**ESTO SIRVE
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-      el = el.trim()
-      if (all) {
-        return [...document.querySelectorAll(el)]
+
+  /**
+   * Easy on scroll event listener TRANSICIONES
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /** Esto hace que en la barra de navegacion, los a cambien de color dependiendo de donde esté el user
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
       } else {
-        return document.querySelector(el)
+        navbarlink.classList.remove('active')
       }
-    }
-  
-  
-  
-    /**ESTO SIRVE
-     * Easy on scroll event listener  TRANSICIONES
-     */
-    const onscroll = (el, listener) => {
-      el.addEventListener('scroll', listener)
-    }
-  
-    /**ESTO SIVE
-     * Aparece en color el icono que esta siendo usado en el nav
-     */
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-      let position = window.scrollY + 200
-      navbarlinks.forEach(navbarlink => {
-        if (!navbarlink.hash) return
-        let section = select(navbarlink.hash)
-        if (!section) return
-        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-          navbarlink.classList.add('active')
-        } else {
-          navbarlink.classList.remove('active')
-        }
-      })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
-  
-  
-  
-    /** ESTO SIRVE
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+/** ESTO SIRVE
      * Flechita Back To top
      */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-      const toggleBacktotop = () => {
-        if (window.scrollY > 100) {
-          backtotop.classList.add('active')
-        } else {
-          backtotop.classList.remove('active')
-        }
-      }
-      window.addEventListener('load', toggleBacktotop)
-      onscroll(document, toggleBacktotop)
+let backtotop = select('.back-to-top')
+if (backtotop) {
+  const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
     }
-  
-  
+  }
+  window.addEventListener('load', toggleBacktotop)
+  onscroll(document, toggleBacktotop)
+}
+
+
   /*ESTO SIRVE*/ 
     /**
      * Animation on scroll
@@ -75,45 +71,49 @@
         mirror: false
       })
     });
-  
-    /**
+
+
+
+
+
+
+
+  /**
      * Easy event listener function
      */
-    const on = (type, el, listener, all = false) => {
-      let selectEl = select(el, all)
-      if (selectEl) {
-        if (all) {
-          selectEl.forEach(e => e.addEventListener(type, listener))
-        } else {
-          selectEl.addEventListener(type, listener)
-        }
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
       }
     }
-  
-   
-      /**ESTO SIRVE
+  }
+/**ESTO SIRVE
      * Mobile nav toggle hace aparecer el nav en modo celu
      */
-      on('click', '.mobile-nav-toggle', function(e) {
-        select('body').classList.toggle('mobile-nav-active')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
-      })
-  
-    // Obtener referencias a los elementos
-  const mobileNavToggle = document.getElementById('mobileNavToggle');
-  const tomasContainer = document.querySelector('.tomas-container');
-  
-  // Agregar un evento de clic al icono de navegación móvil
-  mobileNavToggle.addEventListener('click', function() {
-      // Verificar si el contenedor está actualmente visible
-      if (tomasContainer.style.display !== 'none') {
-          // Si está visible, ocultarlo
-          tomasContainer.style.display = 'none';
-      } else {
-          // Si está oculto, mostrarlo
-          tomasContainer.style.display = 'block';
-      }
-  });
-  
-  })()
+on('click', '.mobile-nav-toggle', function(e) {
+  select('body').classList.toggle('mobile-nav-active')
+  this.classList.toggle('bi-list')
+  this.classList.toggle('bi-x')
+})
+
+// Obtener referencias a los elementos
+const mobileNavToggle = document.getElementById('mobileNavToggle');
+const tomasContainer = document.querySelector('.tomas-container');
+
+// Agregar un evento de clic al icono de navegación móvil
+mobileNavToggle.addEventListener('click', function() {
+// Verificar si el contenedor está actualmente visible
+if (tomasContainer.style.display !== 'none') {
+    // Si está visible, ocultarlo
+    tomasContainer.style.display = 'none';
+} else {
+    // Si está oculto, mostrarlo
+    tomasContainer.style.display = 'block';
+}
+});
+
+})()
